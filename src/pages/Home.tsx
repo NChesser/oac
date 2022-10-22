0fac1251fcf2320564e3656a388b90c9cdf87c18
Nick Chesser <nick.chesser@outlook.com>
2022-10-23 10:23:02 +1030

    Tailwind Finish Setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+/*
+    About: Home OAC Page
+*/
+
+
+const Home = () => {
+
+    return (
+        <div className='w-90 bg-black text-white'>
+            <h1 className="text-3xl font-bold underline ">
+                Hello world!
+            </h1>
+            <p className='text-slate-500'>My Name is Nick</p>
+        </div>
+    )
+};
+
+
+export default Home;
