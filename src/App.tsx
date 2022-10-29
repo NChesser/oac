55cdc2ea082131461f2d492a60d588d72a9166df
Nick Chesser <nick.chesser@outlook.com>
2022-10-29 20:55:15 +1030

    Routing Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
 
+// Routing
+import { Routes, Route, Link } from "react-router-dom";
+
 // Components
 import Home from './pages/Home';
 
@@ -7,7 +10,34 @@ import Home from './pages/Home';
 function App() {
 	return (
 		<div className="App">
-			<Home />
+			<header>
+				<div>
+					<h1>
+						OAC
+					</h1>
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/start'>Start</Link>
+						</li>
+						<li>
+							<Link to='/categories'>Categories</Link>
+						</li>
+						<li>
+							<Link to='/languages'>Languages</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/challenges' element={<Home />} />
+					<Route path='/completed-challenges' element={<p>Completed Challenges</p>} />
+				</Routes>
+			</main>
 		</div>
 	);
 }
